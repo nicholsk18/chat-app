2f8e7abfe12b2e1e24ddcf617b074bf7d5d22c59
Karson Nichols <nicholsk18@gmail.com>
2020-04-01 12:17:41 -0500

    added chat app acknowledgment

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,7 +10,12 @@ send.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const msg = e.target.elements.message.value
-    socket.emit('sendMsg', msg)
+    socket.emit('sendMsg', msg, (error) => {
+        if(error){
+            return console.log(error)
+        }
+        console.log('Message delivered!')
+    })
 })
 
 document.querySelector("#send-location").addEventListener('click', () => {
@@ -22,6 +27,8 @@ document.querySelector("#send-location").addEventListener('click', () => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
+        }, () => {
+            console.log("Location shared")
         })
     })
 })
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -22,14 +23,22 @@ io.on('connection', (socket) => {
     // show to everyone but the user comming from
     socket.broadcast.emit("message", "a new user has joined")
 
-    socket.on('sendMsg', (msg) => {
+    socket.on('sendMsg', (msg, callback) => {
+        const filter = new Filter()
+        
+        if(filter.isProfane(msg)){
+            return callback('Profanity is not allowed!')
+        }
+
         // sends it to everyone
         io.emit('message', msg)
+        callback()
     })
 
     // sends location to users
-    socket.on('sendLocation', (cords) => {
+    socket.on('sendLocation', (cords, callback) => {
         io.emit('message', `https://google.com/maps?q=${cords.latitude},${cords.longitude}`)
+        callback()
     })
 
     // for when user disconects

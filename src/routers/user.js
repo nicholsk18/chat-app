9235c291971397630c3b489cc4428255adb411b7
Karson Nichols <nicholsk18@gmail.com>
2020-04-12 19:35:33 -0500

    got redirect working

--- src/routers/user.js
+++ src/routers/user.js
@@ -5,7 +5,7 @@ const router = new express.Router()
 router.post('/users',  async (req, res) => {
     console.log(req.body)
     
-    res.redirect('chat.html')
+    res.redirect(`chat.html?username=${req.body.username}&room=${req.body.room}`)
 })
 
 module.exports = router

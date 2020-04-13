dcd173249d15b5fb75f8590843649fcecc2041b0
Karson Nichols <nicholsk18@gmail.com>
2020-04-12 21:59:00 -0500

    test

--- src/routers/user.js
+++ src/routers/user.js
@@ -4,8 +4,10 @@ const router = new express.Router()
 
 router.post('/users',  async (req, res) => {
     console.log(req.body)
+    const username = req.body.username
+    const room = req.body.room
     
-    res.redirect(`chat.html?username=${req.body.username}&room=${req.body.room}`)
+    res.redirect(`chat.html?username=${username}&room=${room}`)
 })
 
 module.exports = router
